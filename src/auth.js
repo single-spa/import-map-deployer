b0ca51290693751929022fe94c926c5479ba5550
Nick Humrich <nick.humrich@gmail.com>
2018-03-15 11:59:45 -0600

    Update auth.js

--- src/auth.js
+++ src/auth.js
@@ -15,7 +15,7 @@ module.exports = function(req, res, next) {
     return next()
   }
   var user = auth(req)
-  if (!user || !admins[user.name] || admins[user.name].password !== user.pass) {
+  if ((!user || !admins[user.name] || admins[user.name].password !== user.pass) && req.url != '/') {
     res.set('WWW-Authenticate', 'Basic realm="sofe-deplanifester"')
     return res.status(401).send()
   }
