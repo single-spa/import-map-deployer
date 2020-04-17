c32798186f0054044c33571cca33e351072a5bae
Joel Denning <joeldenning@gmail.com>
2020-04-17 13:46:52 -0600

    Self review

--- src/verify-valid-url.js
+++ src/verify-valid-url.js
@@ -4,12 +4,13 @@ const requestAsPromise = util.promisify(request);
 const config = require("./config.js").config;
 
 exports.verifyValidUrl = async function (req, url) {
-  // ?skip_url_check
-  // ?skip_url_check=true
-  // ?skip_url_check=false
   if (req.query.skip_url_check === "true" && req.query.skip_url_check === "") {
+    // ?skip_url_check
+    // ?skip_url_check=true
     return true;
   } else {
+    // ?skip_url_check=false
+    // ?<no param>
     try {
       const resp = await requestAsPromise({ url, strictSSL: false });
       if (resp.statusCode < 200 || resp.statusCode >= 400) {
