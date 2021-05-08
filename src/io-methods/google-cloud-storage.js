06d3a4fb3c0e9b7aaeedbba6ce82fbb5d5ca2147
Valeri Hristov <valericfbg@gmail.com>
2021-05-08 20:34:03 +0300

    check if file exist and create if not

--- src/io-methods/google-cloud-storage.js
+++ src/io-methods/google-cloud-storage.js
@@ -1,10 +1,12 @@
 const { Storage } = require("@google-cloud/storage");
+const { first } = require("lodash");
 
 const storage = new Storage();
 
 const regex = /^(?:google|gs):\/\/(.+?)\/(.+)$/;
 
 const { getCacheControl } = require("../cache-control");
+const { getEmptyManifest } = require("../modify");
 
 exports.isGooglePath = function (path) {
   return regex.test(path);
@@ -18,15 +20,17 @@ function parseFilePath(filePath) {
   return { bucketName, fileName };
 }
 
-exports.readManifest = function (filePath) {
-  return Promise.resolve().then(() => {
-    const { bucketName, fileName } = parseFilePath(filePath);
-    return storage
-      .bucket(bucketName)
-      .file(fileName)
-      .download()
-      .then((data) => data.toString("utf-8"));
-  });
+exports.readManifest = async function (filePath) {
+  const { bucketName, fileName } = parseFilePath(filePath);
+  const file = await storage.bucket(bucketName).file(fileName);
+
+  const exists = first(await file.exists());
+
+  if (!exists) {
+    await exports.writeManifest(filePath, JSON.stringify(getEmptyManifest()));
+  }
+
+  return file.download().then((data) => data.toString("utf-8"));
 };
 
 exports.writeManifest = function (filePath, data) {
