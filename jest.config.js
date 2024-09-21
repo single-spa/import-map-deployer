7fc4240ac2e40edfda3ebd6f15c09bcadd5dec97
Joel Denning <joeldenning@gmail.com>
2024-09-20 21:24:26 -0600

    Comment

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   testEnvironment: "node",
   moduleNameMapper: {
+    // Latest version of @azure/storage-blob isn't compatible with jest-runtime's implementation
     "@azure/storage-blob": "<rootDir>/__mocks__/azureStorage.js",
   },
 };
